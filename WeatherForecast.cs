1e9aafee1c804ab76f805a493e7bf610200fe393
dukeent <duc31072001@gmail.com>
2022-05-28 15:22:52 +0700

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,17 @@
+namespace ECommerceProject
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+<<<<<<< HEAD
+
+=======
+>>>>>>> 0e4bc454e7cac696edf277787f65a5afb551498d
+    }
+}
